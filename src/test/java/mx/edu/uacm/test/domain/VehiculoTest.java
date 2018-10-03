15b4b91e849c011d90e5238eaafcfba8888e39b6
Profesor <aseguramiento.uacm@gmail.com>
2018-10-03 16:25:03 -0500

    se agregan cambios del dia de hoy
    Agregando cambios de manejador de base de datos, ahora utilizamos mysql

--- src/test/java/mx/edu/uacm/test/domain/VehiculoTest.java
+++ src/test/java/mx/edu/uacm/test/domain/VehiculoTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import mx.edu.uacm.Application;
+import mx.edu.uacm.domain.Vehiculo;
 import mx.edu.uacm.repository.VehiculoRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -20,12 +21,22 @@ public class VehiculoTest {
 	public static final Logger log = 
 			LogManager.getLogger(VehiculoTest.class);
 	
-	@Autowired
+	@Autowired //--> inyeccion de dependencias
 	VehiculoRepository vehiculoRepository;
 	
 	@Test
 	public void testGuardarVehiculo() {
 		log.debug("Entrando a testGuardarVehiculo");
+		
+		//Creando el objeto
+		Vehiculo vehiculo = new Vehiculo();
+		//el id es auto generado por la bd
+		//Agregando el modelo
+		vehiculo.setModelo("2019");
+		//vehiculo.setId(1);
+		
+		//Aqui se guarda el objeto vehiculo en bd
+		vehiculoRepository.save(vehiculo);
 	}
 	
 	
