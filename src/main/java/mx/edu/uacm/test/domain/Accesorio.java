ccbe25a5f2f1f5ef6193a18e2ecea5fb7d5152b6
stardusannie <32911568+stardusannie@users.noreply.github.com>
2018-10-10 14:39:49 -0500

    :3 clase accesorio

---
+++ src/main/java/mx/edu/uacm/test/domain/Accesorio.java
@@ -0,0 +1,11 @@
+package mx.edu.uacm.test.domain;
+
+public class Accesorio {
+	
+	private String nombreAccesorio;
+
+	public Accesorio(String nombreAccesorio) {
+		this.nombreAccesorio = nombreAccesorio;
+	}
+
+}
